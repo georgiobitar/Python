a2c01b112ab35ba5d09d3243ba0eb901c53bf951
georgiobitar <georgiobitar1@hotmail.com>
2021-04-30 21:50:39 +0300

    Creating SlackBot

--- SlackBot/app.py
+++ SlackBot/app.py
@@ -1,15 +1,47 @@
 import os
-
+import random
 from flask import Flask
 from slack import WebClient
 import os
+from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 
-slack_web_client = WebClient(token=os.environ.get("SLACKBOT_TOKEN"))
+slack_events_adapter = SlackEventAdapter(os.environ.get("SLACK_EVENTS_TOKEN2"), "/slack/events")
 
-@app.route('/')
-def hello_world():
-    return 'hello test'
+slack_web_client = WebClient(token=os.environ.get("SLACKBOT_TOKEN2"))
+
+MESSAGE_BLOCK = {
+    "type": "seciton",
+    "text": {
+        "type": "mrkdwn",
+        "text": ""
+    }
+}
+
+@slack_events_adapter.on("message")
+def message(payload):
+
+    event = payload.get("event", {})
+
+    text = event.get("text")
+
+    if "flip a coin" in text.lower():
+
+        channel_id = event.get("channel")
+        rand_int = random.randint(0,1)
+        if rand_int == 0:
+            results = "Heads"
+        else:
+            results = "Tails"
+
+        message = f"The result is {results}"
+
+        MESSAGE_BLOCK["text"]["text"] = message
+        message_to_send = {"channel": channel_id, "blocks":[MESSAGE_BLOCK]}
+
+        return  slack_web_client.chat_postMessage(**message_to_send)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
